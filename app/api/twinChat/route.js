7d132618b41af5aa609f4c3f5430e8e2b6cbf735
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2024-03-23 19:08:09 -0500

    vercel error fix

--- app/api/twinChat/route.js
+++ app/api/twinChat/route.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const sendMessage = async (message) => {
-  try {
-    const response = await axios.post("https://fastapi-rlx1.onrender.com/chat", {
-      message: message,
-    });
-    const answer = response.data;
-    // Process the answer here
-    console.log(answer);
-    return answer; // Return the message from the response
-  } catch (error) {
-    console.error(error);
-  }
-};
+// export const sendMessage = async (message) => {
+//   try {
+//     const response = await axios.post("https://fastapi-rlx1.onrender.com/chat", {
+//       message: message,
+//     });
+//     const answer = response.data;
+//     // Process the answer here
+//     console.log(answer);
+//     return answer; // Return the message from the response
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,60 +1,60 @@
-import axios from 'axios';
-
-export async function POST(Payload) {
-  try {
-    const response = await axios.post('https://fastapi-rlx1.onrender.com/user', Payload);
-
-    if (!response.status === 200) {
-      throw new Error('Request failed with status ' + response.status);
-    }
-
-    const data = response.data;
-
-    return data;
-  } catch (error) {
-    console.error(error);
-
-    // Return the error message as a fallback
-    return 'Internal server error';
-  }
-}
-
-
-// import { NextResponse } from 'next/server';
-
-// // forces to not cache the response to get a different age every request
-// export const dynamic = 'force-dynamic';
+// import axios from 'axios';
 
 // export async function POST(Payload) {
 //   try {
-//     const response = await fetch('http://127.0.0.1:8000/user', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(Payload)
-//     });
-//     console.log("ok")
+//     const response = await axios.post('https://fastapi-rlx1.onrender.com/user', Payload);
 
-//     if (!response.ok) {
+//     if (!response.status === 200) {
 //       throw new Error('Request failed with status ' + response.status);
 //     }
 
-//     const user_data = await response.json();
+//     const data = response.data;
 
-//     return NextResponse.json({
-//       creds: user_data.creds,
-//       age: user_data.age,
-//       dage: user_data.dage,
-//       info: user_data.info
-//     });
+//     return data;
 //   } catch (error) {
 //     console.error(error);
 
-//     return NextResponse.json({
-//       message: 'Internal server error'
-//     }, {
-//       status: 500
-//     });
+//     // Return the error message as a fallback
+//     return 'Internal server error';
 //   }
 // }
+
+
+// // import { NextResponse } from 'next/server';
+
+// // // forces to not cache the response to get a different age every request
+// // export const dynamic = 'force-dynamic';
+
+// // export async function POST(Payload) {
+// //   try {
+// //     const response = await fetch('http://127.0.0.1:8000/user', {
+// //       method: 'POST',
+// //       headers: {
+// //         'Content-Type': 'application/json'
+// //       },
+// //       body: JSON.stringify(Payload)
+// //     });
+// //     console.log("ok")
+
+// //     if (!response.ok) {
+// //       throw new Error('Request failed with status ' + response.status);
+// //     }
+
+// //     const user_data = await response.json();
+
+// //     return NextResponse.json({
+// //       creds: user_data.creds,
+// //       age: user_data.age,
+// //       dage: user_data.dage,
+// //       info: user_data.info
+// //     });
+// //   } catch (error) {
+// //     console.error(error);
+
+// //     return NextResponse.json({
+// //       message: 'Internal server error'
+// //     }, {
+// //       status: 500
+// //     });
+// //   }
+// // }
